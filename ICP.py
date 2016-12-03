26afa00a69d4f6ecc60aa007346cb500382a3547
EC2 Default User <ec2-user@ip-172-31-58-189.ec2.internal>
2016-12-03 07:02:06 +0000

    serial connection

--- ICP.py
+++ ICP.py
@@ -2,12 +2,26 @@
 # December 3, 2016
 # protocol type: polling
 
-import pySerial
+import serial
 
-def hello():
+portAddress = '/dev/ttyUSB0'
+timeout = 1 # seconds
+baudrate = 19200
+port = 'COM1'
+
+def hello(ser, baudrate, timeout):
 	# receives helloandInfo input from sensor
 	# exchanges only at startup
 	# helloandInfo has cmd body of 4 bytes long
+
+    with serial.Serial(portAddress, baudrate, timeout) as ser: # open serial port
+        if (ser.isopen):
+            print('connected to serial' + ser.name)
+            ser.write(b'hello') # write a hello over serial port
+            s = ser.read(8) # read up to 8 bytes: start cmd cmd1 cmd2 cmd3 cmd4 checksum end
+        else:
+            print('error connecting to serial port')
+ 
 # first two bytes: sensor ID number (unique) # next two are version number
 # (major and minor) eg. version 1.5, major = 1, minor = 5, limited to 255 or
 # 2^8
@@ -15,6 +29,7 @@ def hello():
 	# byte 5 and 6 time counter no RTC? time value in two bytes stores
 	# unsigned short = 5 bits day, 5 bits hour, 6 bits minute (shift and mask
 	# to get values) 
+        # verify if we want hour, min, seconds instead
 	
 	unsigned short timestamp = <some value>; // Bits: DDDDDHHHHHMMMMMM
 
@@ -23,15 +38,17 @@ def hello():
 	int minute = (timestamp) & 0x3F;
 
 def measurementSettings():
-	# receives settingsInitialized signal from the sensor
-	# parameters initialized to be used throughout
+    # receives settingsInitialized signal from the sensor
+    # parameters initialized to be used throughout
 
-def queryBattery():
-	# receives battery status
+def queryBattery(ser):
+    # receives battery status
+    
 
-
-def doMeasurement():
-	# receives measurement data
+def doMeasurement(ser):
+    # receives measurement data, continuous reading
+    while (data != None):
+        line = ser.readline() # reads a '\n' terminated line         
 
 def Bye():
 	# sends a bye, whoever received a bye ends session and replies with bye
