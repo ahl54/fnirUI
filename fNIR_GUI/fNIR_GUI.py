219d5a5a85be9a7139eee8e04ac49fc49d3c9bbd
cdcox94 <cdcox94@gmail.com>
2017-02-07 17:49:57 -0500

    Add files via upload

--- fNIR_GUI/fNIR_GUI.py
+++ fNIR_GUI/fNIR_GUI.py
@@ -1,13 +1,12 @@
 import PyQt5
 import ICP
 import sys
-import pyqtgraph as pg
 import atexit
 import numpy as np
 import pyqtgraph as pg
 
 from PyQt5 import QtCore
-from PyQt5.QtWidgets import QWidget,QToolTip,QPushButton,QApplication, QMainWindow, QGridLayout, QGroupBox,QDesktopWidget, QMenuBar, QMainWindow
+from PyQt5.QtWidgets import QWidget,QToolTip,QPushButton,QApplication, QMainWindow, QGridLayout, QGroupBox,QDesktopWidget, QMenuBar, QMainWindow, QMessageBox
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import pyqtSlot, pyqtSignal, QObject, QThread, QTimer
 
@@ -44,6 +43,10 @@ class fNIR_GUI(QMainWindow):
 		self.setWindowTitle('Infrascanner 3000')
 		self.sSize = QtCore.QSize()
 
+		self.timerSize = QTimer()
+		self.timerSize.timeout.connect(self.updateSize)
+		self.timerSize.start(1)
+
 		self.show()
 
 
@@ -53,15 +56,38 @@ class fNIR_GUI(QMainWindow):
 
 		QWidget().setLayout(self.grid)
 		oxyGrid = QGridLayout(self)
-
-		self.mainWind.setLayout(oxyGrid)
-
 		self.plot0 = pg.PlotWidget()
 		self.oxygroup2 = QGroupBox()
+		oxyGrid2 = QGridLayout(self)
+		self.oxygroup2.setLayout(oxyGrid2)
+		self.mainWind.setLayout(oxyGrid)
+
+		self.running = 0
+		self.paused = 0
+		
+		butt4 = QPushButton('Start Measurement')	
+		butt5 = QPushButton('Pause Measurement')
+		butt6 = QPushButton('Clear Graph')
+
+		butt4.clicked.connect(self.strMeas)
+		butt5.clicked.connect(self.pseMeas)
+		butt6.clicked.connect(self.clrGraph)
+
+		oxyGrid2.addWidget(butt4,0,0)
+		oxyGrid2.addWidget(butt5,0,1)
+		oxyGrid2.addWidget(butt6,0,2)
+
 		oxyGrid.addWidget(self.plot0,0,0)
 		oxyGrid.addWidget(self.oxygroup2,0,1)
+
 		self.inOxy = 1
+
 		self.plot0.plot(self.xlin,self.ylin)
+		self.plot0.setLimits(xMin=0,xMax=300,yMin=-3,yMax=3)
+
+		self.timerPlot = QTimer()
+		self.timerPlot.timeout.connect(self.updatePlot)
+
 		self.show()
 
 	def updateSize(self):
@@ -71,15 +97,46 @@ class fNIR_GUI(QMainWindow):
 		if (self.inOxy==1):
 			self.plot0.move(5,5)
 			self.plot0.resize(self.frameGeometry().width()/2-10, self.frameGeometry().height()-75)
+
 			self.oxygroup2.move(self.frameGeometry().width()/2, 5)
 			self.oxygroup2.resize(self.frameGeometry().width()/2-30, self.frameGeometry().height()-75)
 
 	def updatePlot(self):
 		if(self.inOxy==1):
+
 			self.ylin[0:299]=self.ylin[1:300]
 			self.ylin[299] = np.random.normal(size=1)
+			#self.ylin[299] = myICP.pollData()
+
 			self.plot0.clear()
 			self.plot0.plot(self.xlin,self.ylin)
+
+	def strMeas(self):
+		
+		if((self.running==0)&(self.paused==0)):
+			self.myICP = ICP.ICP()
+			if(self.myICP.serCon==1):
+				self.myICP.sayHello()
+				self.myICP.measSettings()
+			else:
+				self.alertUser = QMessageBox()
+				self.alertUser.setText("Could not connect to Infrascanner 3000")
+				self.alertUser.setIcon(3)
+				self.alertUser.setWindowTitle("Error")
+				self.alertUser.exec_()
+
+		elif (self.paused==1):
+			self.timerPlot.start(1000)
+			self.paused=0
+			self.running=1
+
+	def pseMeas(self):
+		if(self.running ==1):
+			self.timerPlot.stop()
+			self.paused = 1
+
+	def clrGraph(self):
+		self.plot0.clear()
 
 
 def main():
@@ -87,12 +144,7 @@ def main():
 	app = QApplication(sys.argv)
 
 	ex = fNIR_GUI()
-	timerSize = QTimer()
-	timerSize.timeout.connect(ex.updateSize)
-	timerSize.start(1)
-	timerPlot = QTimer()
-	timerPlot.timeout.connect(ex.updatePlot)
-	timerPlot.start(1000)
+	
 	sys.exit(app.exec_())
 
 		
